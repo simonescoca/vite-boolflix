6ac02af1653eb34f67c165cd94cf4982635e899b
simonescoca <simone.scoca@icloud.com>
2023-06-21 15:26:58 +0200

    added props from array MoviesData

---
+++ src/components/SingleMovie.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" :alt="`Copertina di ${movie.title}`">
+        <div class="card-body">
+            <h5 class="card-title">
+                Titolo: {{ movie.title }}
+            </h5>
+            <h5>
+                Titolo originale: {{ movie.original_title }}
+            </h5>
+            <h5>
+                Lingua originale: {{ movie.original_language }}
+            </h5>
+            <h5>
+                Voto: {{ movie.vote_average }}
+            </h5>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import axios from "axios"
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        components: {
+
+        },
+
+        props: {
+            movie: Object
+        },
+
+        mounted () {
+
+        },
+
+        created () {
+
+        },
+
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "../styles/variables";
+    @use "../styles/mixins";
+</style>
