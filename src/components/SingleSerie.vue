b8ff52c9d3e4b765b874ab82c2c2630ea4227bfc
simonescoca <simone.scoca@icloud.com>
2023-06-21 17:58:41 +0200

    created seriescontainer & singleserie

---
+++ src/components/SingleSerie.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="card">
+        <img src="..." class="card-img-top" :alt="`Copertina di ${serie.name}`">
+        <div class="card-body">
+            <h5 class="card-title">
+                Titolo: {{ serie.name }}
+            </h5>
+            <h5>
+                Titolo originale: {{ serie.original_name }}
+            </h5>
+            <div class="d-flex align-items-center justify-content-start my_gap">
+                <div>
+                    Lingua originale:
+                </div>
+                <div class="my_flag-container">
+                    <img :src="checkFlag(serie.original_language)" :alt="serie.original_language">
+                </div>
+            </div>
+            <h5>
+                Voto: {{ serie.vote_average }}
+            </h5>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import axios from "axios"
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        components: {
+
+        },
+
+        props: {
+            serie: Object,
+            flags: Object
+        },
+
+        mounted () {
+
+        },
+
+        created () {
+
+        },
+
+        methods: {
+            checkFlag (language) {
+                let a = "this.flags." + language
+                a = eval(a)
+                return a
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "../styles/variables";
+    @use "../styles/mixins";
+
+    .card {
+        width: 18rem;
+    }
+
+    .my_flag-container {
+        height: 25px;
+        width: 25px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: contain;
+            object-position: center;
+        }
+    }
+
+    .my_gap {
+        gap: 1rem;
+    }
+</style>
