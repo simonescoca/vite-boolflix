37b91cd6653d7c8d87853815bac3353a572d993f
simonescoca <simone.scoca@icloud.com>
2023-06-21 15:26:41 +0200

    added a v-for & a prop "movie" to his child

---
+++ src/components/MoviesContainer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container d-flex flex-wrap">
+       <SingleMovie v-for="movie in moviesData" :movie="movie"/>
+    </div>
+</template>
+
+<script>
+    import SingleMovie from "./SingleMovie.vue"
+    export default {
+        data() {
+            return {
+
+            }
+        },
+
+        components: {
+            SingleMovie
+        },
+
+        props: {
+            moviesData: Array
+        },
+
+        mounted () {
+
+        },
+
+        created () {
+
+        },
+
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use "../styles/variables";
+    @use "../styles/mixins";
+
+    .container {
+        gap: 2rem;
+    }
+</style>
