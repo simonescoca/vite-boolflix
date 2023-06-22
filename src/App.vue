753ef0f22e517656aeb7a2399bab5ff272ab80bf
simonescoca <simone.scoca@icloud.com>
2023-06-22 16:29:43 +0200

    added background color to app.vue and an overflow auto

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <AppHeader />
-    <AppMain />
+    <div class="my_background">
+        <AppHeader />
+        <div class="my_main-box">
+            <AppMain />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -39,4 +43,15 @@
 <style lang="scss">
     @import "bootstrap/scss/bootstrap";
     @import "./styles/general.scss";
+
+    .my_background {
+        background-color: #1b1b1b;
+        height: 100vh;
+    }
+
+    .my_main-box {
+        height: calc(100vh - 196px);
+        overflow: auto;
+        scroll-behavior: smooth;
+    }
 </style>
